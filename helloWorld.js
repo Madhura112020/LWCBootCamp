88262fe7ee900df1fbc661c314f646a90a55c42f
Madhura112020 <74545462+Madhura112020@users.noreply.github.com>
2020-11-18 17:58:28 +0530

    Add files via upload

---
+++ helloWorld.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+export default class HelloWorld extends LightningElement {
+.blue{
+    color:blue
+}
+.red{
+    color:red
+}
+.green{
+    color:green
+}
+ul:not(.browser-default) li {
+    list-style-type: disc;
+}
+.card{
+    width:50%;
+}
+}
